1c1e4ebea303d2535ecc8fbb54c6533086cf773b
davidwsk <davidwsk@hotmail.com>
2017-09-15 14:20:15 +0800

    1ts Commit

---
+++ app/src/main/java/com/iscdasia/smartjlptn5_android/Question.java
@@ -0,0 +1,111 @@
+package com.iscdasia.smartjlptn5_android;
+
+import com.microsoft.windowsazure.mobileservices.table.DateTimeOffset;
+
+/**
+ * Created by iscd-dev01 on 14/9/2017.
+ */
+
+public class Question {
+    public Question() {
+    }
+
+    @com.google.gson.annotations.SerializedName("deleted")
+    private Boolean mDeleted;
+    public Boolean getDeleted() { return  mDeleted;}
+    public final void setDeleted(Boolean deleted){ mDeleted = deleted;}
+    public boolean isDeleted() {
+        return mDeleted;
+    }
+
+    /// <summary>
+    /// Id for item
+    /// </summary>
+    @com.google.gson.annotations.SerializedName("id")
+    private String mId;
+    public String getId() { return mId; }
+    public final void setId(String id) { mId = id; }
+
+    @com.google.gson.annotations.SerializedName("questionGroupId")
+    private String mQuestionGroupId;
+
+    public String getQuestionGroupId() {
+        return mQuestionGroupId;
+    }
+
+    public final void setQuestionGroupId(String mQuestionGroupId) {
+        this.mQuestionGroupId = mQuestionGroupId;
+    }
+
+    @com.google.gson.annotations.SerializedName("questionText")
+    private String mQuestionText;
+
+    public String getQuestionText() {
+        return mQuestionText;
+    }
+
+    public final void setQuestionText(String mQuestionText) {
+        this.mQuestionText = mQuestionText;
+    }
+
+    @com.google.gson.annotations.SerializedName("correctAnswer")
+    private String mCorrectAnswer;
+
+    public String getCorrectAnswer() {
+        return mCorrectAnswer;
+    }
+
+    public final void setCorrectAnswer(String mCorrectAnswer) {
+        this.mCorrectAnswer = mCorrectAnswer;
+    }
+
+    @com.google.gson.annotations.SerializedName("wrongAnswer1")
+    private String mWrongAnswer1;
+
+    public String getWrongAnswer1() {
+        return mWrongAnswer1;
+    }
+
+    public final void setWrongAnswer1(String mWrongAnswer1) {
+        this.mWrongAnswer1 = mWrongAnswer1;
+    }
+
+    @com.google.gson.annotations.SerializedName("wrongAnswer2")
+    private String mWrongAnswer2;
+
+    public String getWrongAnswer2() {
+        return mWrongAnswer2;
+    }
+
+    public final void setWrongAnswer2(String mWrongAnswer2) {
+        this.mWrongAnswer2 = mWrongAnswer2;
+    }
+
+    @com.google.gson.annotations.SerializedName("wrongAnswer3")
+    private String mWrongAnswer3;
+
+    public String getWrongAnswer3() {
+        return mWrongAnswer3;
+    }
+
+    public final void setWrongAnswer3(String mWrongAnswer3) {
+        this.mWrongAnswer3 = mWrongAnswer3;
+    }
+
+    @com.google.gson.annotations.SerializedName("description")
+    private String mDescription;
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public final void setDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Question && ((Question) o).mId == mId;
+    }
+}
---
+++ app/src/main/java/com/iscdasia/smartjlptn5_android/ToDoActivity.java
@@ -0,0 +1,495 @@
+package com.iscdasia.smartjlptn5_android;
+
+
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.http.NextServiceFilterCallback;
+import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilter;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterRequest;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+import com.microsoft.windowsazure.mobileservices.table.sync.MobileServiceSyncContext;
+import com.microsoft.windowsazure.mobileservices.table.sync.localstore.ColumnDataType;
+import com.microsoft.windowsazure.mobileservices.table.sync.localstore.MobileServiceLocalStoreException;
+import com.microsoft.windowsazure.mobileservices.table.sync.localstore.SQLiteLocalStore;
+import com.microsoft.windowsazure.mobileservices.table.sync.synchandler.SimpleSyncHandler;
+import com.squareup.okhttp.OkHttpClient;
+
+import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.*;
+
+public class ToDoActivity extends Activity {
+
+    /**
+     * Mobile Service Client reference
+     */
+    private MobileServiceClient mClient;
+
+    /**
+     * Mobile Service Table used to access data
+     */
+    //private MobileServiceTable<Question> mToDoTable;
+
+    private MobileServiceTable<Question> mQuetionTable;
+
+    //Offline Sync
+    /**
+     * Mobile Service Table used to access and Sync data
+     */
+    //private MobileServiceSyncTable<Question> mToDoTable;
+
+    /**
+     * Adapter to sync the items list with the view
+     */
+    private ToDoItemAdapter mAdapter;
+
+    /**
+     * EditText containing the "New To Do" text
+     */
+    private EditText mTextNewToDo;
+
+    /**
+     * Progress spinner to use for table operations
+     */
+    private ProgressBar mProgressBar;
+
+    /**
+     * Initializes the activity
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.loadingProgressBar);
+
+        // Initialize the progress bar
+        mProgressBar.setVisibility(ProgressBar.GONE);
+
+        try {
+            // Create the Mobile Service Client instance, using the provided
+
+            // Mobile Service URL and key
+            mClient = new MobileServiceClient(
+                    "https://jlptn5g.azurewebsites.net",
+                    this).withFilter(new ProgressFilter());
+
+            // Extend timeout from default of 10s to 20s
+            mClient.setAndroidHttpClientFactory(new OkHttpClientFactory() {
+                @Override
+                public OkHttpClient createOkHttpClient() {
+                    OkHttpClient client = new OkHttpClient();
+                    client.setReadTimeout(20, TimeUnit.SECONDS);
+                    client.setWriteTimeout(20, TimeUnit.SECONDS);
+                    return client;
+                }
+            });
+
+            // Get the Mobile Service Table instance to use
+
+            //mToDoTable = mClient.getTable(ToDoItem.class);
+            mQuetionTable = mClient.getTable(Question.class);
+
+            // Offline Sync
+            //mToDoTable = mClient.getSyncTable("Question", Question.class);
+
+            //Init local storage
+            initLocalStore().get();
+
+            mTextNewToDo = (EditText) findViewById(R.id.textNewToDo);
+
+            // Create an adapter to bind the items with the view
+            mAdapter = new ToDoItemAdapter(this, R.layout.row_list_to_do);
+            ListView listViewToDo = (ListView) findViewById(R.id.listViewToDo);
+            listViewToDo.setAdapter(mAdapter);
+
+            // Load the items from the Mobile Service
+            refreshItemsFromTable();
+
+        } catch (MalformedURLException e) {
+            createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
+        } catch (Exception e){
+            createAndShowDialog(e, "Error");
+        }
+    }
+
+    /**
+     * Initializes the activity menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+
+    /**
+     * Select an option from the menu
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_refresh) {
+            refreshItemsFromTable();
+        }
+
+        return true;
+    }
+
+    /**
+     * Mark an item as completed
+     *
+     * @param question
+     *            The item to mark
+     */
+    public void checkItem(final Question question) {
+        if (mClient == null) {
+            return;
+        }
+
+        // Set the item as completed and update it in the table
+        question.setDeleted(true);
+
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+
+                    checkItemInTable(question);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (question.isDeleted()) {
+                                mAdapter.remove(question);
+                            }
+                        }
+                    });
+                } catch (final Exception e) {
+                    createAndShowDialogFromTask(e, "Error");
+                }
+
+                return null;
+            }
+        };
+
+        runAsyncTask(task);
+
+    }
+
+    /**
+     * Mark an item as completed in the Mobile Service Table
+     *
+     * @param question
+     *            The item to mark
+     */
+    public void checkItemInTable(Question question) throws ExecutionException, InterruptedException {
+        mQuetionTable.update(question).get();
+    }
+
+    /**
+     * Add a new item
+     *
+     * @param view
+     *            The view that originated the call
+     */
+    public void addItem(View view) {
+        if (mClient == null) {
+            return;
+        }
+
+        // Create a new item
+        final Question item = new Question();
+
+        item.setQuestionText(mTextNewToDo.getText().toString());
+        item.setDeleted(false);
+
+        // Insert the new item
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    final Question entity = addItemInTable(item);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(!entity.isDeleted()){
+                                mAdapter.add(entity);
+                            }
+                        }
+                    });
+                } catch (final Exception e) {
+                    createAndShowDialogFromTask(e, "Error");
+                }
+                return null;
+            }
+        };
+
+        runAsyncTask(task);
+
+        mTextNewToDo.setText("");
+    }
+
+    /**
+     * Add an item to the Mobile Service Table
+     *
+     * @param item
+     *            The item to Add
+     */
+    public Question addItemInTable(Question item) throws ExecutionException, InterruptedException {
+        Question entity = mQuetionTable.insert(item).get();
+        return entity;
+    }
+
+    /**
+     * Refresh the list with the items in the Table
+     */
+    private void refreshItemsFromTable() {
+
+        // Get the items that weren't marked as completed and add them in the
+        // adapter
+
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+
+                try {
+                    final List<Question> results = refreshItemsFromMobileServiceTable();
+
+                    //Offline Sync
+                    //final List<Question> results = refreshItemsFromMobileServiceTableSyncTable();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mAdapter.clear();
+
+                            for (Question item : results) {
+                                mAdapter.add(item);
+                            }
+                        }
+                    });
+                } catch (final Exception e){
+                    createAndShowDialogFromTask(e, "Error");
+                }
+
+                return null;
+            }
+        };
+
+        runAsyncTask(task);
+    }
+
+    /**
+     * Refresh the list with the items in the Mobile Service Table
+     */
+
+    private List<Question> refreshItemsFromMobileServiceTable() throws ExecutionException, InterruptedException {
+        return mQuetionTable.where().field("deleted").
+                eq(val(false)).execute().get();
+    }
+
+    //Offline Sync
+    /**
+     * Refresh the list with the items in the Mobile Service Sync Table
+     */
+    /*private List<Question> refreshItemsFromMobileServiceTableSyncTable() throws ExecutionException, InterruptedException {
+        //sync the data
+        sync().get();
+        Query query = QueryOperations.field("complete").
+                eq(val(false));
+        return mToDoTable.read(query).get();
+    }*/
+
+    /**
+     * Initialize local storage
+     * @return
+     * @throws MobileServiceLocalStoreException
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private AsyncTask<Void, Void, Void> initLocalStore() throws MobileServiceLocalStoreException, ExecutionException, InterruptedException {
+
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+
+                    MobileServiceSyncContext syncContext = mClient.getSyncContext();
+
+                    if (syncContext.isInitialized())
+                        return null;
+
+                    SQLiteLocalStore localStore = new SQLiteLocalStore(mClient.getContext(), "OfflineStore", null, 1);
+
+                    Map<String, ColumnDataType> tableDefinition = new HashMap<String, ColumnDataType>();
+                    tableDefinition.put("id", ColumnDataType.String);
+                    tableDefinition.put("questionText", ColumnDataType.String);
+                    tableDefinition.put("deleted", ColumnDataType.Boolean);
+
+                    localStore.defineTable("Question", tableDefinition);
+
+                    SimpleSyncHandler handler = new SimpleSyncHandler();
+
+                    syncContext.initialize(localStore, handler).get();
+
+                } catch (final Exception e) {
+                    createAndShowDialogFromTask(e, "Error");
+                }
+
+                return null;
+            }
+        };
+
+        return runAsyncTask(task);
+    }
+
+    //Offline Sync
+    /**
+     * Sync the current context and the Mobile Service Sync Table
+     * @return
+     */
+    /*
+    private AsyncTask<Void, Void, Void> sync() {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    MobileServiceSyncContext syncContext = mClient.getSyncContext();
+                    syncContext.push().get();
+                    mToDoTable.pull(null).get();
+                } catch (final Exception e) {
+                    createAndShowDialogFromTask(e, "Error");
+                }
+                return null;
+            }
+        };
+        return runAsyncTask(task);
+    }
+    */
+
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param exception
+     *            The exception to show in the dialog
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialogFromTask(final Exception exception, String title) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowDialog(exception, "Error");
+            }
+        });
+    }
+
+
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param exception
+     *            The exception to show in the dialog
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialog(Exception exception, String title) {
+        Throwable ex = exception;
+        if(exception.getCause() != null){
+            ex = exception.getCause();
+        }
+        createAndShowDialog(ex.getMessage(), title);
+    }
+
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param message
+     *            The dialog message
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialog(final String message, final String title) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.create().show();
+    }
+
+    /**
+     * Run an ASync task on the corresponding executor
+     * @param task
+     * @return
+     */
+    private AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        } else {
+            return task.execute();
+        }
+    }
+
+    private class ProgressFilter implements ServiceFilter {
+
+        @Override
+        public ListenableFuture<ServiceFilterResponse> handleRequest(ServiceFilterRequest request, NextServiceFilterCallback nextServiceFilterCallback) {
+
+            final SettableFuture<ServiceFilterResponse> resultFuture = SettableFuture.create();
+
+
+            runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+            });
+
+            ListenableFuture<ServiceFilterResponse> future = nextServiceFilterCallback.onNext(request);
+
+            Futures.addCallback(future, new FutureCallback<ServiceFilterResponse>() {
+                @Override
+                public void onFailure(Throwable e) {
+                    resultFuture.setException(e);
+                }
+
+                @Override
+                public void onSuccess(ServiceFilterResponse response) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.GONE);
+                        }
+                    });
+
+                    resultFuture.set(response);
+                }
+            });
+
+            return resultFuture;
+        }
+    }
+}
