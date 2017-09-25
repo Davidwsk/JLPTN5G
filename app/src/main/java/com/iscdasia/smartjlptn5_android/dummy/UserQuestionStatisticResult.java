dcc943a422000ac6677f2f3f5f74baa142e394aa
davidwsk <davidwsk@hotmail.com>
2017-09-25 13:33:58 +0800

    Thet Commit

---
+++ app/src/main/java/com/iscdasia/smartjlptn5_android/dummy/UserQuestionStatisticResult.java
@@ -0,0 +1,31 @@
+package com.iscdasia.smartjlptn5_android.dummy;
+
+/**
+ * Created by iscd-dev01 on 25/9/2017.
+ */
+
+public class UserQuestionStatisticResult {
+    private String CurrentResult;
+    private String AllResult;
+
+    public UserQuestionStatisticResult(String currentResult, String allResult) {
+        CurrentResult = currentResult;
+        AllResult = allResult;
+    }
+
+    public String getCurrentResult() {
+        return CurrentResult;
+    }
+
+    public void setCurrentResult(String currentResult) {
+        CurrentResult = currentResult;
+    }
+
+    public String getAllResult() {
+        return AllResult;
+    }
+
+    public void setAllResult(String allResult) {
+        AllResult = allResult;
+    }
+}
